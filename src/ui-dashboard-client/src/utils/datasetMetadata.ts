2aa648bea690d92dbd81b472552602bbbcfb854d
am202 <am202@regenstrief.org>
2022-10-27 13:58:28 -0400

    Fix undefined errors.

--- src/ui-dashboard-client/src/utils/datasetMetadata.ts
+++ src/ui-dashboard-client/src/utils/datasetMetadata.ts
@@ -12,19 +12,23 @@ export interface DatasetMetadataColumns {
 export const getDatasetMetadataColumns = (meta: DatasetMetadata): DatasetMetadataColumns => {
     const output: DatasetMetadataColumns = {};
 
-    if (meta.ref.shape == PatientListDatasetShape.Dynamic) {
-            const dynSchema = meta.schema as PatientListDatasetDynamicSchema;
-            if (dynSchema.sqlFieldDate)         { output.fieldDate = dynSchema.sqlFieldDate; }
-            if (dynSchema.sqlFieldValueString)  { output.fieldValueString = dynSchema.sqlFieldValueString; }
-            if (dynSchema.sqlFieldValueNumeric) { output.fieldValueNumeric = dynSchema.sqlFieldValueNumeric; }
-            return output;
-    } else {
-        const def = DefTemplates.get(meta.ref.shape);
-        if (def) {
-            if (def.dateValueColumn)    { output.fieldDate = def.dateValueColumn; }
-            if (def.stringValueColumn)  { output.fieldValueString = def.stringValueColumn; }
-            if (def.numericValueColumn) { output.fieldValueNumeric = def.numericValueColumn; }
+    if(!!meta && !!meta.ref) {
+        if (meta.ref.shape == PatientListDatasetShape.Dynamic) {
+                const dynSchema = meta.schema as PatientListDatasetDynamicSchema;
+                if (dynSchema.sqlFieldDate)         { output.fieldDate = dynSchema.sqlFieldDate; }
+                if (dynSchema.sqlFieldValueString)  { output.fieldValueString = dynSchema.sqlFieldValueString; }
+                if (dynSchema.sqlFieldValueNumeric) { output.fieldValueNumeric = dynSchema.sqlFieldValueNumeric; }
+                
+        } else {
+            const def = DefTemplates.get(meta.ref.shape);
+            if (def) {
+                if (def.dateValueColumn)    { output.fieldDate = def.dateValueColumn; }
+                if (def.stringValueColumn)  { output.fieldValueString = def.stringValueColumn; }
+                if (def.numericValueColumn) { output.fieldValueNumeric = def.numericValueColumn; }
+            }
+            
         }
-        return output;
     }
+    return output;
+
 };
